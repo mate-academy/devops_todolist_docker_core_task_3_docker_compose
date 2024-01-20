f3f2850a750a0b0857c0f3f2b6297a5ae96180a6
ladmael <don.sosiska339@gmail.com>
2024-01-20 21:40:35 +0200

    fixed

--- Dockerfile
+++ Dockerfile
@@ -22,5 +22,4 @@ EXPOSE 8080
 
 COPY entrypoint.sh /entrypoint.sh
 
-# Make sure we make the entrypoint.sh script executable
 RUN chmod +x /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
-# Stop on the first sign of trouble
-set -e
 
-apt-get update
-apt-get install -y netcat-openbsd
-
-# Check for the db to be ready
-echo "Waiting for MySQL..."
-while ! nc -z mysql 3306; do
-  sleep 0.1
-done
-echo "MySQL started"
-# Start server
-echo "Starting server"
-python manage.py runserver 0.0.0.0:8080
-
-# Apply database migrations
-echo "Applying database migrations"
 python manage.py migrate
+
+python manage.py runserver 0.0.0.0:8080
